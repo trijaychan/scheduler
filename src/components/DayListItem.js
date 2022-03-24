a54656b379dd1bdc6839fa4703bcf7eea9b447d4
trijaychan <trijaychan@gmail.com>
2022-03-24 10:33:27 -0700

    Refactored code to account for new prop names in parent element

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,6 +4,7 @@ import classNames from "classnames"
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const { setDay } = props;
   const classes = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
@@ -11,16 +12,16 @@ export default function DayListItem(props) {
 
   const formatSpots = (spots) => {
     if (spots === 0) {
-      return "no spots remaining";
+      return "No spots remaining";
     } else if (spots === 1) {
       return "1 spot remaining";
     } else {
       return `${spots} spots remaining`;
     }
-  }
+  };
 
   return (
-    <li className={classes} onClick={() => props.setDay(props.name)}>
+    <li className={classes} onClick={setDay}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
