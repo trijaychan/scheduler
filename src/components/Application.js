0fa6af157e8d94e8da04f44e70ffd18e0fea00b8
trijaychan <trijaychan@gmail.com>
2022-03-25 10:01:53 -0700

    Added intereviewers to state object and appointments now show interviewer name

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,13 +4,14 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const setDay = (day) => setState({ ...state, day });
@@ -18,19 +19,24 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
       axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments")
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers")
     ]).then((all) => {
-      setState({ ...state, days: all[0].data, appointments: all[1].data});
+      setState({ ...state, days: all[0].data, appointments: all[1].data, interviewers: all[2].data });
     })
   }, []);
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
 
-  const aptList = Object.values(dailyAppointments).map((appointment) => {
+  const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
     return (
-      <Appointment 
+      <Appointment
         key={appointment.id}
-        {...appointment}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
       />
     );
   });
@@ -59,7 +65,7 @@ export default function Application(props) {
       </section>
 
       <section className="schedule">
-        {aptList}
+        {schedule}
       </section>
     </main>
   );
