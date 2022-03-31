27c31635d39448eebebec3ed231d4befe7d668b6
trijaychan <trijaychan@gmail.com>
2022-03-30 20:40:16 -0700

    Fixed issue where tests weren't passing because name was used instead of student for props

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -24,7 +24,7 @@ describe("Form", () => {
 
   it("renders with initial student name", () => {
     const { getByTestId } = render(
-      <Form interviewers={interviewers} name="Lydia Miller-Jones" />
+      <Form interviewers={interviewers} student="Lydia Miller-Jones" />
     );
     expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
   });
@@ -69,7 +69,7 @@ describe("Form", () => {
     const { getByText, getByPlaceholderText, queryByText } = render(
       <Form
         interviewers={interviewers}
-        name="Lydia Mill-Jones"
+        student="Lydia Mill-Jones"
         onSave={jest.fn()}
         onCancel={onCancel}
       />
