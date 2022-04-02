26d32771239b345fb12be3b3c375cd9b1f1e45d2
trijaychan <trijaychan@gmail.com>
2022-04-02 13:41:03 -0700

    Now destructures props

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -5,6 +5,8 @@ import DayListItem from "./DayListItem";
 // component that display days where appointments can be booked and the 
 // number of spots available
 export default function DayList(props) {
+  const { value, onChange } = props;
+
   // creates an array consisting of weekdays using an array which is passed
   // as props
   const listItems = props.days.map((day) => {
@@ -13,8 +15,8 @@ export default function DayList(props) {
         key={day.id} 
         name={day.name} 
         spots={day.spots} 
-        selected={day.name === props.value}
-        setDay={() => props.onChange(day.name)}
+        selected={day.name === value}
+        setDay={() => onChange(day.name)}
       />
     );
   });
