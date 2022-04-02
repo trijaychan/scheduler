326d9b7a86c5b3cd986d189b134a17c832a725db
trijaychan <trijaychan@gmail.com>
2022-04-02 13:37:40 -0700

    Form cannot be submitted without a selected interviewer now

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -25,6 +25,10 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
+    if (!interviewer) {
+      setError("Interviewer cannot be blank");
+      return;
+    }
     
     setError("");
     props.onSave(student, interviewer);
